8ea235014a13599ef759f522faa5e9d4ab1c614d
dnrjs <93022330+dnrjs@users.noreply.github.com>
2021-11-08 02:15:18 +0900

    js: project filter

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else{
@@ -16,8 +14,7 @@ document.addEventListener('scroll', () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
+    const link = event.target.dataset.link;
     if(link == null){
         return;
     }
@@ -53,7 +50,28 @@ arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 })
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    projectContainer.classList.add('anim-out');
 
+    setTimeout(() => {
+        projects.forEach((project) => {
+        if(filter === '*' || filter === project.dataset.type){
+        project.classList.remove('invisible');
+    } else{
+        project.classList.add('invisible');
+    }
+    });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
 
 
 
